98f40c745a20a6e82ed4076e8c2138278cb0eccc
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2014-03-09 13:09:39 +0900

    up

--- momo_sp/spritz.js
+++ momo_sp/spritz.js
@@ -104,7 +104,7 @@ function spritzify(input){
         }
 
         // Double up on long words and words with commas.
-        if((all_words[i].indexOf(',') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf('-') != -1 || all_words[i].indexOf('(') != -1|| all_words[i].length > 8) && all_words[i].indexOf('.') == -1){
+        if((all_words[i].indexOf(',') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf('-') != -1 || all_words[i].indexOf('(') != -1|| all_words[i].length > 8) && all_words[i].indexOf('•') == -1){
             temp_words.splice(t+1, 0, all_words[i]);
             temp_words.splice(t+1, 0, all_words[i]);
             t++;
